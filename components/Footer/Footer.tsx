d1792365867474f1ab0652f48499296a31a3bd94
Coşkun Atak <coskntkk@gmail.com>
2023-06-07 11:54:50 +0300

    add buymeacoffee link

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -10,6 +10,7 @@ const Footer = () => {
                 <a href="https://github.com/Coskntkk">Github</a>
                 <a href="https://github.com/Coskntkk/public-gallery">Repository</a>
                 <a href="https://twitter.com/dimaetor">Twitter</a>
+                <a href="https://www.buymeacoffee.com/coskntkk" target="_blank" rel="noreferrer" style={{color: "yellow"}}>Buy Me A Coffee</a>
             </div>
         </div>
     )
